31f590f6a2615484e557d5d91cad621e8614c931
Pedro Elias Subia <subia.p@opendevpro.com>
2025-04-23 16:20:39 -0300

    first commit

---
+++ src/main/java/com/opendev/external_services/ExternalServicesApplication.java
@@ -0,0 +1,13 @@
+package com.opendev.external_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalServicesApplication.class, args);
+	}
+
+}
